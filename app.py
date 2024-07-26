5d2ae4df0819d4af88801bf448ee1a8b33219513
Atenas <atekapeko@gmail.com>
2024-07-25 23:24:00 -0400

    ADD: Download music function

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 # ----- IMPORT ----- #
+#os
+import os
 # kivy
 import kivy
 kivy.require('2.3.0')
@@ -8,20 +10,13 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.properties import ColorProperty
-
 # pytube
 from pytubefix import YouTube
+# ffmpeg-python
+import ffmpeg
 
 # ----- FUNCTIONS ----- #
-def convertVideo(url):
-    # Create YouTube instance
-    yt = YouTube(url)
 
-    # Get stream
-    stream = yt.streams.filter(only_audio=True).first()
-    print(yt.title)
-    return yt.title
-    # streams: itag, mime_type, abr, codecs, type
 
 
 
@@ -35,16 +30,39 @@ class InputSection(GridLayout):
         self.padding = 8
         self.rows = 3
 
+        # Input for video URL
         self.inputUrl = TextInput(multiline=False)
         self.add_widget(Label(text='Enter video URL:'))
         self.add_widget(self.inputUrl)
-        downloadButton = Button(text='Download')
-        self.add_widget(downloadButton)
-   
-        def callback():
-            return convertVideo(self.inputUrl.text)
+
+        # Download button
+        self.downloadButton = Button(text='Download')
+        self.downloadButton.bind(on_press=self.downloadMusic)
+        self.add_widget(self.downloadButton)
+
+    def downloadMusic(self, instance):
+        url = self.inputUrl.text
         
-        downloadButton.bind(on_press=callback)
+        # Instance YouTube object
+        yt = YouTube(url)
+
+        # Get stream
+        stream = yt.streams.filter(only_audio=True).first()
+
+        # Get filename without blank spaces
+        filename = yt.title.replace(' ', '')
+
+        # Download file
+        stream.download(mp3=True, filename=filename, output_path='mp3')
+        
+        # Convert mp4a to mp3
+        stream = ffmpeg.input(f'mp3/{filename}.mp3')
+        stream = ffmpeg.output(stream, f'mp3/{filename}-mp3.mp3')
+        ffmpeg.run(stream, overwrite_output=True)
+        os.remove(f'mp3/{filename}.mp3')
+
+
+
 
 
 
